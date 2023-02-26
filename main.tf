9c6bb558b2ae16e208c13a6ddd0b1fd7d1a641ce
thurundil <thurundil@mail.com>
2023-02-26 14:04:27 +0100

    Fix duplicated resources

--- main.tf
+++ main.tf
@@ -27,8 +27,6 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
-resource "random_pet" "sg" {}
-
 resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
